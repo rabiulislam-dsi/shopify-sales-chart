fc8c5dc64b583b3b909eb9f2334c45c819d33378
Rabiul Islam Anik <rabiulislam.dsi@gmail.com>
2021-02-01 15:04:03 +0600

    Add privacy policy

---
+++ pages/privacy.js
@@ -0,0 +1,42 @@
+import { Layout, Page } from '@shopify/polaris';
+
+const PrivacyPolicy = () => (
+  <Page>
+    <Layout>
+    Sales Chart Privacy Policy
+
+    Sales Chart "the App” provides Sales Metrics "the Service" to merchants who use Shopify to power their stores. This Privacy Policy describes how personal information is collected, used, and shared when you install or use the App in connection with your Shopify-supported store.
+
+    Personal Information the App Collects
+
+    When you install the App, we are automatically able to access certain types of information from your Shopify account: 
+        'read_products',
+        'read_product_listings',
+        'read_customers',
+        'read_orders',
+        'read_draft_orders',
+        'read_locations',
+        'read_fulfillments',
+        'read_assigned_fulfillment_orders',
+        'read_merchant_managed_fulfillment_orders',
+        'read_third_party_fulfillment_orders',
+        'read_analytics'
+
+    Additionally, we collect the following types of personal information from you and/or your customers once you have installed the App: 
+    Information about you and others who may access the App on behalf of your store, such as your name, address, email address, phone number, and billing information; 
+    Information about individuals who visit your store,  web browser details, time zone, and information about the cookies installed on the particular device. 
+
+    We collect personal information directly from the relevant individual, through your Shopify account, or using the following technologies: “Cookies” are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org. “Log files” track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps. “Web beacons,” “tags,” and “pixels” are electronic files used to record information about how you browse the Site.
+
+    How Do We Use Your Personal Information?
+
+    We use the personal information we collect from you and your customers in order to provide the Service and to operate the App. Additionally, we use this personal information to: Communicate with you; Optimize or improve the App; and Provide you with information or advertising relating to our products or services.
+
+    Changes We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.
+
+    Contact Us For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by e-mail at rabiul.islam@dsinnovators.com .
+    </Layout>
+  </Page>
+);
+
+export default PrivacyPolicy;
